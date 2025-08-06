fa924f833b81db2dfaa31aa902e9fa1383d99119
AbdalrhmanAmmar <bodyty2n@gmail.com>
2025-08-06 05:00:09 +0300

    next-congif

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+    ],
+    // إذا كنت تستخدم صور محلية خارج app
+    domains: ['localhost'],
+  },
+
+};
 
 export default nextConfig;
