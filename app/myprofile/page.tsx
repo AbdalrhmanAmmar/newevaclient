0a8a30fec19dbfafb3501fe70c956da84226ee3d
AbdalrhmanAmmar <bodyty2n@gmail.com>
2025-08-11 06:18:50 +0300

    fix-profile-err

--- app/myprofile/page.tsx
+++ app/myprofile/page.tsx
@@ -1,5 +1,7 @@
 import ProfileClient from "@/components/profile/ProfileClient";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default function MyProfilePage() {
   return <ProfileClient />;
