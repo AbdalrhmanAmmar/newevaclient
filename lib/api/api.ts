5b25644efcbc1924d7c07aa340fe75c091ffcb18
AbdalrhmanAmmar <bodyty2n@gmail.com>
2025-08-23 17:00:22 +0300

    edit-url-backend

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // إعداد Axios
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://back-api.evasaudi.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
